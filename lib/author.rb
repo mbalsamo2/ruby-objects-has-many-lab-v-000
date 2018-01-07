12aaa44a56cce95e00596a454f9efd9ffb18751f
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-07 18:39:23 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,15 @@
 class Author
   attr_accessor :name, :posts
 
+
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def add_post(post)
+    @posts << self.post
+    
   end
 
 end
